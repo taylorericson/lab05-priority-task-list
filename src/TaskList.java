e3179fd169cb65dc897dba9f64584632ed2d10d7
Taylor Ericson <116030067+taylorericson@users.noreply.github.com>
2025-03-29 20:18:21 -0500

    Update TaskList.java

--- src/TaskList.java
+++ src/TaskList.java
@@ -10,21 +10,21 @@ import java.util.List;
 
 public class TaskList {
 
-	public static void main(String[] args) {
-		// Create new Task objects
-		Task t1, t2, t3, t4, t5;
+    public static void main(String[] args) {
+	// Create new Task objects
+	Task t1, t2, t3, t4, t5;
 		
-		// Initialize Task objects with a name and priority
-		t1 = new Task("Attend class", 1);
+	// Initialize Task objects with a name and priority
+	t1 = new Task("Attend class", 1);
         t2 = new Task("Homework", 2);
     	t3 = new Task("Exercise", 3);
     	t4 = new Task("Eat breakfast", 4);
     	t5 = new Task("Eat lunch", 5);
 		
-		// Create a list of Task objects
-		List<Task> tasks = new ArrayList<>();
+	// Create a list of Task objects
+	List<Task> tasks = new ArrayList<>();
 		
-		// Add tasks to tasks list
+	// Add tasks to tasks list
         tasks.add(t1);
         tasks.add(t2);
     	tasks.add(t3);
@@ -32,13 +32,13 @@ public class TaskList {
     	tasks.add(t5);
 
         System.out.println("Priority Task List\n" +
-        				   "------------------\n");
+        		   "------------------\n");
         
         // Loop through the list and print each task
         for (Task task : tasks) {
             System.out.println(task);
         }
 
-	}
+    }
 
 }
